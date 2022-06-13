fad80af7b871fb812511cfdfb86b5bbace3da252
ahsinrasheed <ahsinbhatti5@gmail.com>
2022-06-13 12:17:48 +0500

    Query middleware and aggregate middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema({
         required: [true, 'A tour must have a name'],
         unique: true,
         trim: true,
+        maxlength: [40, 'A tour Name must have less or equal then 40 characters'], 
+        minlength: [10, 'A tour Name must have less or equal then 10 characters'], 
     },
     slug: String,
     duration: {
@@ -21,10 +23,16 @@ const tourSchema = new mongoose.Schema({
     difficulty: {
         type: String,
         required: [true, 'A tour must have a Difficulty'],
+        enum: {
+            value: ['easy', 'medium', 'difficulty'],
+            message: 'Difficulty is either: easy, medium or difficulty',
+        }
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
         type: Number,
@@ -55,6 +63,10 @@ const tourSchema = new mongoose.Schema({
         select : false
     },
     startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false,
+    }
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
@@ -83,7 +95,28 @@ tourSchema.pre('save', function (next) {
 //     next();
 // })
 
-  
+// QUERY MIDDLEWARE ::
+
+tourSchema.pre(/^find/, function (next) {
+    this.find({ secretTour: { $ne: true } });
+    this.start = Date.now();
+    next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+    console.log(`Query tool ${Date.now() - this.start} milliseconds`);  
+    next();
+});
+
+// Aggregation MIDDLEWARE
+
+tourSchema.pre('aggregate', function (next) {
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+    console.log(this);
+    next();
+})
+
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
