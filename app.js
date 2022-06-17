1d53c1df9c1d72432c1ad0a4bed61eee54d3c1ec
ahsinrasheed <ahsinbhatti5@gmail.com>
2022-06-17 21:55:28 +0500

    installing new packages and  Preventing Parameter Pollution

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -32,6 +35,26 @@ app.use('/api', limiter);
 
 // BODY Parser, Reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data Sanitization  against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whiteList: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 // Serving Static FIles
 app.use(express.static(`${__dirname}/public`));
