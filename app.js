1ab16576f0ca7659ca60c95404a6621168980d2e
ahsinrasheed <ahsinbhatti5@gmail.com>
2022-06-17 19:07:16 +0500

    Implementing Rate limiting and setting security http headers

--- app.js
+++ app.js
@@ -2,22 +2,41 @@
 /* eslint-disable import/extensions */
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
-
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// MiddleWare
+// 1) GLOBAL MiddleWare
+
+// Set Security HTTP Headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, Please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// BODY Parser, Reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving Static FIles
 app.use(express.static(`${__dirname}/public`));
 
+// Test MIDDLEWARE
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
