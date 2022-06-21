ec575ee280fd5b33899ef6a03a76e65e4aa735bc
ahsinrasheed <ahsinbhatti5@gmail.com>
2022-06-21 14:34:22 +0500

    Improving read performance with indexes

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -72,6 +72,7 @@ exports.getAll = (Model) =>
       .limitFields()
       .pagination();
 
+    // const doc = await features.query.explain();
     const doc = await features.query;
 
     // SEND RESPONSE
