82519b7857f2f353193cc415153a2e064919c41a
ahsinrasheed <ahsinbhatti5@gmail.com>
2022-06-22 17:10:22 +0500

    Creating Our Base Template

--- app.js
+++ app.js
@@ -71,7 +71,10 @@ app.use((req, res, next) => {
 
 // Mount the Routes...
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Ahsin',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
