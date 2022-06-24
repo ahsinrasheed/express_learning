2a5722f2d4eb5a6e533238c187b55a61dbab8cd0
ahsinrasheed <ahsinbhatti5@gmail.com>
2022-06-24 10:00:32 +0500

    Building thr Tour Overview page by template pug

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,11 +1,32 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get Tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Building template
+  // 3) Render that template using tour data from 1)
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) Get Tour data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'reviews rating user',
+  });
+
+  // 2) Build Template
+
+  // 3) Render that template using tour data from 1)
+
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const viewController = require('../controllers/viewController'); 
+const viewController = require('../controllers/viewController');
 
 const router = express.Router();
 
 router.get('/', viewController.getOverview);
-router.get('/tour', viewController.getTour);
+router.get('/tour/:slug', viewController.getTour);
 
 module.exports = router;
