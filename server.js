bcc83f601a966a0c456c11e3535e68c1d8cf0445
ahsinrasheed <ahsinbhatti5@gmail.com>
2022-07-04 11:27:15 +0500

    Responding to a SIGTERM Signal

--- server.js
+++ server.js
@@ -50,10 +50,4 @@ process.on('SIGTREM', () => {
     console.log('Process Terminated!');
   })
 })
-
-
-
-
-
-
 // npm i eslint prettier eslint-config-prettier eslint-plugin-prettier eslint-config-airbnb eslint-plugin-node eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react  --save-dev
