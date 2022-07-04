ad4f6147425b495b9e53286e0ccc32a749416c92
ahsinrasheed <ahsinbhatti5@gmail.com>
2022-07-04 14:59:44 +0500

    Improved Stripe Implementation part 2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,7 +54,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 }
 
@@ -65,18 +65,17 @@ exports.webhookCheckout = (req, res, next) => {
     event = stripe.webhooks.constructEvent(
       req.body,
       signature,
-      process.env.STRIPE_WEBHOOK_SERET
+      process.env.STRIPE_WEBHOOK_SECRET
     );
   } catch (err) { 
-    return res.status(400).send('webook Error: ${err.message}');
+    return res.status(400).send('webhook Error: ${err.message}');
   } 
 
-  if (event.type === 'checkout.session.complete')
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
     res.status(200).json({ received: true });
 
 };
-
 
 exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
