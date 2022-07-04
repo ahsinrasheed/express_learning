ca3917a2e420cda90a8f7dcf74f99381fd66b60f
ahsinrasheed <ahsinbhatti5@gmail.com>
2022-07-04 16:12:37 +0500

    complete the project

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,13 +5,18 @@ const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
+router.use(viewController.alerts);
+
 router.get('/', authController.isLoggedIn, viewController.getOverview);
 
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
 router.get('/me', authController.protect, viewController.getAccount);
 
-router.get('/my-tours',
+
+
+router.get(
+  '/my-tours',
   // bookingController.createBookingCheckout,
   authController.protect,
   viewController.getMyTours
